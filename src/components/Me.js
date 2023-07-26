2f79aee3ca1d06e31b95a52e3b4bb5b01b83bd40
patrickmelan <pjmelan@gmail.com>
2023-07-25 21:36:01 -0400

    Initializes React App using NPX; Themes Tab to Name & Logo

---
+++ src/components/Me.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Me() {
+
+    return (
+        <>
+            <h1 className="font-bold underline">Hi! I am Patrick Melan</h1>
+            <h2>I am an Intern @ St. Lukes University Health Network</h2>
+            <h3>I am a Student @ Easton Area High School</h3>
+        </>
+    )
+}
+
+export default Me;
